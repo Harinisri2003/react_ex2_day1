c00d7b40a12ab84777f45e96eafa4109a5e1014b
Harinisri2003 <108522448+Harinisri2003@users.noreply.github.com>
2023-03-31 10:02:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Reference Data Types use  same memory</h1>
+        <button onClick={window['check']}>Click here to call function</button>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
